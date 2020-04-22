536e57b794804c68466f1b9032b370c00bb56525
Adrien Vidal <adriensg.vidal@gmail.com>
2020-04-22 14:50:16 +0200

    create filmDetails compo

---
+++ Components/FilmDetails.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class FilmDetails extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Details </Text>
+            </View>
+        )
+    }
+}
+
+export default FilmDetails
--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
 import Search from "../Components/Search";
+import FilmDetails from "../Components/FilmDetails";
 
 const Stack = createStackNavigator();
 
@@ -8,9 +9,7 @@ export default function MyStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Rechercher" component={Search} />
-      {/* <Stack.Screen name="Notifications" component={Notifications} />
-      <Stack.Screen name="Profile" component={Profile} />
-      <Stack.Screen name="Settings" component={Settings} /> */}
+      <Stack.Screen name="FilmDetails" component={FilmDetails} />
     </Stack.Navigator>
   );
 }
