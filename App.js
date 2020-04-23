4879115c054de8da43c0a51447c733df6244c7eb
Adrien Vidal <adriensg.vidal@gmail.com>
2020-04-23 17:29:17 +0200

    add redux, store and reducer favoriteFilm

--- App.js
+++ App.js
@@ -3,11 +3,15 @@ import "react-native-gesture-handler";
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigation from "./Navigation/Navigation";
+import { Provider } from "react-redux";
+import Store from "./Store/configureStore";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Navigation />
+      <Provider store={Store}>
+        <Navigation />
+      </Provider>
     </NavigationContainer>
   );
 }
