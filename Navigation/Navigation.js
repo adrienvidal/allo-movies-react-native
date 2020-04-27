97d6c8b60f2480b63e760884ee7c32ee52680c01
Adrien Vidal <adriensg.vidal@gmail.com>
2020-04-27 14:09:11 +0200

    add tabNavigator

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,15 +1,70 @@
-import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { StyleSheet, Image } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Search from "../Components/Search";
 import FilmDetail from "../Components/FilmDetail";
+import Favorites from "../Components/Favorites";
 
 const Stack = createStackNavigator();
-
-export default function MyStack() {
+function MyStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Search" component={Search} options={{ title: 'Rechercher' }}/>
-      <Stack.Screen name="FilmDetail" component={FilmDetail} options={{ title: 'Film Détails' }}/>
+      <Stack.Screen
+        name="Search"
+        component={Search}
+        options={{ title: "Rechercher" }}
+      />
+      <Stack.Screen
+        name="FilmDetail"
+        component={FilmDetail}
+        options={{ title: "Film Détails" }}
+      />
     </Stack.Navigator>
   );
 }
+
+const Tab = createBottomTabNavigator();
+function MoviesTabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let sourceImage;
+          if (route.name === "Search") {
+            sourceImage = require("../Images/ic_search.png");
+          } else if (route.name === "Favorites") {
+            sourceImage = require("../Images/ic_favorite.png");
+          }
+
+          return <Image source={sourceImage} style={styles.favorite_image} />;
+        },
+      })}
+      tabBarOptions={{
+        showLabel: false,
+        activeBackgroundColor: '#DDDDDD',
+        inactiveBackgroundColor: "#FFFFFF"
+      }}
+    >
+      <Tab.Screen
+        name="Search"
+        component={MyStack}
+        options={{ title: "Rechercher" }}
+      />
+      <Tab.Screen
+        name="Favorites"
+        component={Favorites}
+        options={{ title: "Favoris" }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  favorite_image: {
+    width: 30,
+    height: 30,
+  },
+});
+
+export default MoviesTabNavigator;
